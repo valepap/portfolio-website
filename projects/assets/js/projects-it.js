c608c1aecce7a17fdae6f87573c2b0f7832daaa9
Alessandra <alessandra.casole92@gmail.com>
2024-01-07 23:02:19 +0100

    ita

---
+++ projects/assets/js/projects-it.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  // navbar
+  if (window.innerWidth < 992) {
+    openDrop.setAttribute("data-bs-toggle", "dropdown");
+    openDrop.classList.add("dropdown-toggle");
+    openDrop.removeAttribute("href");
+  } else {
+    openDrop.classList.remove("dropdown-toggle");
+    openDrop.href = "../../index-ita.html#projects";
+  }
+
+  $("#carouselExampleIndicators").swipe({
+    swipe: function (
+      event,
+      direction,
+      distance,
+      duration,
+      fingerCount,
+      fingerData
+    ) {
+      if (direction === "left") {
+        $(this).carousel("next");
+      }
+      if (direction === "right") {
+        $(this).carousel("prev");
+      }
+    },
+    allowPageScroll: "vertical",
+  });
+});
