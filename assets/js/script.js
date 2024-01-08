6acaa60cedaababf377f2cc21916ea0ccbc07ba3
Alessandra <alessandra.casole92@gmail.com>
2024-01-08 22:55:08 +0100

    animation scroll-reveal

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,4 +116,88 @@ document.addEventListener("DOMContentLoaded", () => {
     contactForm.classList.remove("was-validated");
     successMessage.style.display = "none";
   }
+
+  /* Scroll Reveal */
+  ScrollReveal({
+    reset: true,
+    distance: "60px",
+    duration: 2300,
+    delay: 100,
+  });
+
+  ScrollReveal().reveal(".hero-txt", {
+    delay: 0,
+    easing: "cubic-bezier(0.5, 0, 0, 1)",
+    // easing: "cubic-bezier(.215, .61, .355, 1)",
+    // interval: 240,
+    scale: 0.65,
+  });
+
+  // Projects
+  ScrollReveal().reveal(".services-section .card, .projects-section .figure", {
+    delay: 500,
+    easing: "cubic-bezier(.215, .61, .355, 1)",
+    interval: 200,
+    scale: 0.65,
+  });
+
+  // Fade Down +
+  ScrollReveal().reveal(".scroll-down", {
+    delay: 200,
+    origin: "top",
+    easing: "ease-in-out",
+    duration: 1500,
+  });
+
+  // Fade Down
+  ScrollReveal().reveal(
+    ".about-section h2, .services-section h2, .projects-section h2, .contact-section h2, .carousel-section .hero-txt ",
+    {
+      delay: 300,
+      origin: "top",
+    }
+  );
+
+  // View More
+  ScrollReveal().reveal(".btnSect, .sub-txt", {
+    delay: 300,
+  });
+
+  ScrollReveal().reveal(".contact-form, .contact-section .on-light", {
+    delay: 400,
+  });
+
+  // Fade left
+  ScrollReveal().reveal(".portfolio-pic, .footer-section p", {
+    delay: 300,
+    origin: "left",
+  });
+
+  // right - desktop only
+  ScrollReveal().reveal(".right-reveal", {
+    delay: 400,
+    origin: "right",
+    mobile: false,
+  });
+
+  //right - Mobile only
+  ScrollReveal().reveal(".right-reveal", {
+    delay: 400,
+    desktop: false,
+  });
+
+  //right - diff - desktop only
+  ScrollReveal().reveal(".right-diff-reveal", {
+    delay: 300,
+    origin: "right",
+    interval: 200,
+    mobile: false,
+  });
+
+  // right - diff - mobile only
+  ScrollReveal().reveal(".right-diff-reveal", {
+    delay: 300,
+    interval: 200,
+    desktop: false,
+  });
 });
--- projects/assets/js/projects-it.js
+++ projects/assets/js/projects-it.js
@@ -27,4 +27,51 @@ $(document).ready(function () {
     },
     allowPageScroll: "vertical",
   });
+
+  /* Scroll Reveal */
+  ScrollReveal({
+    reset: true,
+    distance: "60px",
+    duration: 2300,
+    delay: 100,
+  });
+
+  // Fade Down
+  ScrollReveal().reveal(".carousel-section h1 ", {
+    delay: 300,
+    origin: "top",
+  });
+
+  // Fade left - desktop only
+  ScrollReveal().reveal(".left-reveal", {
+    delay: 450,
+    origin: "left",
+    mobile: false,
+  });
+
+  //Fade right - Mobile only
+  ScrollReveal().reveal(".left-reveal", {
+    delay: 450,
+    desktop: false,
+  });
+
+  // right - desktop only
+  ScrollReveal().reveal(".right-reveal", {
+    delay: 500,
+    origin: "right",
+    mobile: false,
+  });
+
+  //right - Mobile only
+  ScrollReveal().reveal(".right-reveal", {
+    delay: 500,
+    desktop: false,
+  });
+
+  ScrollReveal().reveal(".carousel", {
+    delay: 400,
+    easing: "cubic-bezier(0.5, 0, 0, 1)",
+    // interval: 240,
+    scale: 0.65,
+  });
 });
