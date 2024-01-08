94f176aac039816a71f1013aa8eed6e5b951ee9e
Alessandra <alessandra.casole92@gmail.com>
2024-01-08 17:10:27 +0100

    add prj

--- projects/assets/js/projects.js
+++ projects/assets/js/projects.js
@@ -28,29 +28,29 @@ $(document).ready(function () {
     allowPageScroll: "vertical",
   });
 
-  // Assegna l'ID corretto del video
-  let videoElement = document.getElementById("videoElement");
-  let videoSlide = document.getElementById("videoSlide");
+  // // Assegna l'ID corretto del video
+  // let videoElement = document.getElementById("videoElement");
+  // let videoSlide = document.getElementById("videoSlide");
 
-  // Verifica se l'elemento video esiste prima di aggiungere gli eventi
-  if (videoElement) {
-    // Nascondi l'anteprima quando il video è completamente caricato
-    videoElement.addEventListener("canplaythrough", function () {
-      videoElement.style.display = "block";
-      if (videoSlide.querySelector("img")) {
-        videoSlide.removeChild(videoSlide.querySelector("img"));
-      }
-      // Imposta l'attributo 'controls' solo quando il video è completamente caricato
-      videoElement.setAttribute("controls", "controls");
-    });
+  // // Verifica se l'elemento video esiste prima di aggiungere gli eventi
+  // if (videoElement) {
+  //   // Nascondi l'anteprima quando il video è completamente caricato
+  //   videoElement.addEventListener("canplaythrough", function () {
+  //     videoElement.style.display = "block";
+  //     if (videoSlide.querySelector("img")) {
+  //       videoSlide.removeChild(videoSlide.querySelector("img"));
+  //     }
+  //     // Imposta l'attributo 'controls' solo quando il video è completamente caricato
+  //     videoElement.setAttribute("controls", "controls");
+  //   });
 
-    // Mostra l'anteprima quando il video inizia a caricare
-    videoElement.addEventListener("loadstart", function () {
-      videoElement.style.display = "none";
-      let previewImage = document.createElement("img");
-      previewImage.src = "./assets/img/anteprima.jpeg";
-      previewImage.classList.add("w-100");
-      videoSlide.appendChild(previewImage);
-    });
-  }
+  //   // Mostra l'anteprima quando il video inizia a caricare
+  //   videoElement.addEventListener("loadstart", function () {
+  //     videoElement.style.display = "none";
+  //     let previewImage = document.createElement("img");
+  //     previewImage.src = "./assets/img/anteprima.jpeg";
+  //     previewImage.classList.add("w-100");
+  //     videoSlide.appendChild(previewImage);
+  //   });
+  // }
 });
